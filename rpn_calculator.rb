edd3fd4dfa416c76dbf9077fdb0367146529a002
smspezzano <smspezzano@gmail.com>
2014-03-18 10:20:29 -0700

    WIP: implementing the evalutate method

--- rpn_calculator.rb
+++ rpn_calculator.rb
@@ -6,14 +6,58 @@ class RPNCalculator
  
   # Parse should return a list class that you defined, not a ruby array 
   def self.parse(rpn_string)
+    @new_stack = Stack.new
+    rpn_string.split.each {|item| @new_stack.push(item) }
   end
 
   def self.evaluate(rpn_list)
+    index = 0
+    stack = Stack.new
+
+    while index < rpn_list.length
+      item = rpn_list[index]
+
+      if RPNCalculator.is_number item
+        stack.push item.to_f
+      elsif RPNCalculator.is_operation item
+        right_num = stack.pop
+        left_num = stack.pop
+
+        unless left_num.is_a? Numeric and right_num.is_a? Numeric
+          raise ArgumentError
+        end
+
+        if item == '^'
+          val = left_num.public_send :**, right_num
+        else
+          val = left_num.public_send item.to_sym, right_num
+        end
+        stack.push val
+      end
+
+      index += 1
+    end
+
+    ret_val = stack.pop
+
+    ret_val
   end
 
   def self.is_operation(operation)
+    if /^[\^*+-\/]$/.match(operation) != nil
+      true
+    else
+      false
+    end
   end
   
   def self.is_number(num)
+    # /^[+-]?([\d])+[.]?[\d]*$/.match(num) != nil ? true : false
+
+    if /^[0-9]+$/.match(num) != nil
+      true
+    else
+      false
+    end
   end
 end
--- stack.rb
+++ stack.rb
@@ -1,5 +1,54 @@
 # The stack should be implemented using a linked list.
 # Do not use any ruby arrays!!
+class Node
+  attr_accessor :next, :previous, :value
+  def initialize(value)
+    @value = value
+  end 
+end
 
 class Stack
+
+  attr_reader :length
+
+  def initialize
+    @head = nil
+    @tail = nil
+    @length = 0
+  end
+
+  # Adds a new value to the end of the list.
+  def push(value)
+    if @head.nil?
+      @head = Node.new(value)
+      @tail = @head
+    else
+      old_tail = @tail
+      @tail = Node.new(value)
+      old_tail.next = @tail
+      @tail.previous = old_tail
+    end
+    @length += 1
+  end
+
+  # Returns the value that is popped off
+  # or nil if none exists
+  def pop
+    if @length == 0
+      return nil
+    elsif @length == 1
+      last_tail = @tail
+      @tail = nil
+      @head = nil
+      @length = 0
+      return last_tail.value
+    else     
+      last_tail = @tail
+      @tail = @tail.previous
+      @tail.next = nil
+      @length -= 1
+      last_tail.value
+    end
+  end
+
 end
